21c338f821c3f43f51c0209d91f869e8adb53280
unknown <brenomoraes97@yahoo.com>
2023-03-22 01:42:45 -0300

    classs anatomy

---
+++ src/SchoolReport.java
@@ -0,0 +1,15 @@
+public class SchoolReport {
+
+    public static void main(String[] args) {
+        int finalMedia = 6;
+
+        if (finalMedia < 6)
+            System.out.println("REPROVADO");
+        else if (finalMedia == 6)
+            System.out.println("PROVA DE MINERVA");
+        else
+            System.out.println("APROVADO");
+
+    }
+
+}
